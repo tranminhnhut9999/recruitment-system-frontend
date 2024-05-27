dcbcfed43c15dd545719064a2dcad6aa772c067e
tranminhnhut14079999 <tranminhnhut14079999@gmail.com>
2024-05-28 00:54:02 +0700

    add personal information tab

--- src/app/components/dashboard/account-profile/account-profile.component.ts
+++ src/app/components/dashboard/account-profile/account-profile.component.ts
@@ -1,11 +1,66 @@
 import {Component} from '@angular/core';
 
 @Component({
-    selector: 'app-account-profile',
-    templateUrl: './account-profile.component.html',
-    styleUrls: ['./account-profile.component.scss']
+  selector: 'app-account-profile',
+  templateUrl: './account-profile.component.html',
+  styleUrls: ['./account-profile.component.scss']
 })
 export class AccountProfileComponent {
-    departments: any[] = [{value: "Bộ phận IT"}, {value: "Bộ phận kế toán"}, {value: "Bộ phận bảo vệ"}, {value: "Bộ phận bán hàng"}];
-    workingPlaces: any[] = [{value: "1460 Đ. Võ Văn Kiệt, Phường 1, Quận 6, Thành phố Hồ Chí Minh"}, {value: "Đường Nguyễn Văn Cừ, Hoà Hiệp Bắc, Liên Chiểu, Đà Nẵng"}, {value: "273 Nguyễn Tri Phương, Hòa Thuận Đông, Hải Châu, Đà Nẵng 550000"}];
+  departments: any[] = [{value: "Bộ phận IT"}, {value: "Bộ phận kế toán"}, {value: "Bộ phận bảo vệ"}, {value: "Bộ phận bán hàng"}];
+  workingPlaces: any[] = [{value: "1460 Đ. Võ Văn Kiệt, Phường 1, Quận 6, Thành phố Hồ Chí Minh"}, {value: "Đường Nguyễn Văn Cừ, Hoà Hiệp Bắc, Liên Chiểu, Đà Nẵng"}, {value: "273 Nguyễn Tri Phương, Hòa Thuận Đông, Hải Châu, Đà Nẵng 550000"}];
+  vietnamCities: any[] = [
+    {"value": "Hanoi"},
+    {"value": "Ho Chi Minh City"},
+    {"value": "Da Nang"},
+    {"value": "Hai Phong"},
+    {"value": "Can Tho"},
+    {"value": "Bien Hoa"},
+    {"value": "Nha Trang"},
+    {"value": "Hue"},
+    {"value": "Vung Tau"},
+    {"value": "Quy Nhon"},
+    {"value": "Rach Gia"},
+    {"value": "Thai Nguyen"},
+    {"value": "Nam Dinh"},
+    {"value": "Buon Ma Thuot"},
+    {"value": "Vinh"},
+    {"value": "Ha Long"},
+    {"value": "Thanh Hoa"},
+    {"value": "Phan Thiet"},
+    {"value": "Cam Ranh"},
+    {"value": "Long Xuyen"},
+    {"value": "Thai Binh"},
+    {"value": "Dong Hoi"},
+    {"value": "Sa Dec"},
+    {"value": "Bac Lieu"},
+    {"value": "Bac Giang"},
+    {"value": "Bac Ninh"},
+    {"value": "Ben Tre"},
+    {"value": "Cao Lanh"},
+    {"value": "Da Lat"},
+    {"value": "Dong Ha"},
+    {"value": "Dong Xoai"},
+    {"value": "Ha Giang"},
+    {"value": "Ha Tinh"},
+    {"value": "Hoa Binh"},
+    {"value": "Hung Yen"},
+    {"value": "Kon Tum"},
+    {"value": "Lao Cai"},
+    {"value": "Long Khanh"},
+    {"value": "Mong Cai"},
+    {"value": "My Tho"},
+    {"value": "Phan Rang-Thap Cham"},
+    {"value": "Phu Ly"},
+    {"value": "Pleiku"},
+    {"value": "Quang Ngai"},
+    {"value": "Son La"},
+    {"value": "Tam Ky"},
+    {"value": "Tan An"},
+    {"value": "Tay Ninh"},
+    {"value": "Tuyen Quang"},
+    {"value": "Uong Bi"},
+    {"value": "Vi Thanh"},
+    {"value": "Vinh Long"},
+    {"value": "Yen Bai"}
+  ];
 }
