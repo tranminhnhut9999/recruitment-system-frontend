48c0cabf0ddfbab994af01c9efb7f6fb66fbcabf
tranminhnhut14079999 <tranminhnhut14079999@gmail.com>
2024-05-29 23:37:07 +0700

    adđing job detail for application

---
+++ src/app/components/job-card/job-card.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-job-card',
+  templateUrl: './job-card.component.html',
+  styleUrls: ['./job-card.component.scss']
+})
+export class JobCardComponent {
+  values: any[] = ["Hồ Chí Minh", "Ứng tuyeenr"]
+}
--- src/app/components/job-list/job-list.component.ts
+++ src/app/components/job-list/job-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-job-list',
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./job-list.component.scss']
 })
 export class JobListComponent {
-
+  departments: any[] = [{value: "Bộ phận IT"}, {value: "Bộ phận kế toán"}, {value: "Bộ phận bảo vệ"}, {value: "Bộ phận bán hàng"}]
 }
