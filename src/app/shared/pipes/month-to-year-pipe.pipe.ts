fe655521e9715f13ce7905dfe0b0ff148fbd0fbe
tranminhnhut14079999 <tranminhnhut14079999@gmail.com>
2024-08-11 18:01:14 +0700

    fix typo message of month to year pipe

--- src/app/shared/pipes/month-to-year-pipe.pipe.ts
+++ src/app/shared/pipes/month-to-year-pipe.pipe.ts
@@ -9,9 +9,7 @@ export class MonthToYearPipePipe implements PipeTransform {
     if (months >= 12) {
       const years = Math.floor(months / 12);
       const remainingMonths = months % 12;
-      const yearPart = `${years} năm`;
-      const monthPart = remainingMonths > 0 ? `${remainingMonths} tháng` : '';
-      return yearPart && monthPart ? `${yearPart} ${monthPart}` : `${yearPart}${monthPart}`;
+      return `${years},${remainingMonths} năm`;
     } else {
       return `${months} tháng`;
     }
